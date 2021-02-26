e725682314d63e354a74c5d4608e74665c2c54ca
Kyungin Jin <kyungin2@gmail.com>
2021-02-26 10:30:34 +0900

    add Pair of Parentheses

---
+++ Dynamic/Parentheses/ParenthesesRecur.java
@@ -0,0 +1,39 @@
+package Dynamic.Parentheses;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author : 진경인
+ * @version : 1.0
+ * @since : 2021. 02. 26 오전 10:13
+ */
+public class ParenthesesRecur {
+
+    Set <String> generatePatterns(int remaining) {
+
+        Set<String> set = new HashSet <>();
+
+        if(remaining == 0) {
+            set.add("");
+        } else {
+            Set<String> prev = generatePatterns(remaining - 1);
+            for (String str : prev ) {
+                for (int i= 0; i < str.length() ; i++ ) {
+                    if ( str.charAt(i) == '(') {
+                        String s  = insertInside(str, i);
+                        set.add(s);
+                    }
+                }
+            }
+        }
+        return set;
+    }
+
+    String insertInside(String str, int leftIndex) {
+        String left = str.substring(0, leftIndex + 1) ;
+        String right = str.substring(leftIndex + 1, str.length() ) ;
+        return left + "()" + right;
+    }
+
+}
---
+++ Dynamic/Parentheses/ParenthesesRecurAdven.java
@@ -0,0 +1,32 @@
+package Dynamic.Parentheses;
+
+import java.util.ArrayList;
+
+/**
+ * @author : 진경인
+ * @version : 1.0
+ * @since : 2021. 02. 26 오전 10:14
+ */
+public class ParenthesesRecurAdven {
+
+    void addParentheses(ArrayList <String> list, int leftRem, int rightRem, char[] str, int index ) {
+        if (leftRem < 0 || rightRem < leftRem ) return;
+        if (leftRem == 0 && rightRem == 0 ) {
+            list.add(String.copyValueOf(str));
+        } else {
+            str[index] = '(';
+            addParentheses(list, leftRem - 1, rightRem, str, index + 1 );
+
+            str[index] = ')';
+            addParentheses(list, leftRem, rightRem + 1, str, index + 1 );
+        }
+    }
+
+    ArrayList<String> generateParentheses(int count) {
+        char[] str = new char[count * 2] ;
+        ArrayList<String> list = new ArrayList<String>();
+        addParentheses(list, count, count, str, 0);
+        return list;
+    }
+
+}
