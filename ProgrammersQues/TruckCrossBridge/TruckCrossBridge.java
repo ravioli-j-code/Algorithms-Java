cf31a26826e5e6079f63ac1a7ac1aa9188c386e2
Kyungin Jin <kyungin2@gmail.com>
2021-03-10 14:16:19 +0900

    complete truck cross bridge

--- ProgrammersQues/TruckCrossBridge/TruckCrossBridge.java
+++ ProgrammersQues/TruckCrossBridge/TruckCrossBridge.java
@@ -11,66 +11,76 @@ import java.util.*;
  */
 public class TruckCrossBridge {
 
+    class Truck {
+        int weight;
+        int move;
+
+        public Truck(int weight) {
+            this.weight = weight;
+            this.move = 1;
+        }
+
+        public void moveTruck() {
+            move++;
+        }
+
+    }
+
     int runTrucks(int bridge_length, int weight, int[] truck_weights) {
 
         if (bridge_length == 0 && weight == 0) return 0;
         if (truck_weights.length == 0) return 0;
 
-        Queue<Integer> trucksOnBridge = new LinkedList<>();
-        for (int i = 0 ; i < truck_weights.length ; i++ ) {
-            if ( truck_weights[i] > weight ) { // 넘어갈수 있는 트럭인지 확인
+        Queue<Truck> waiting = new LinkedList<>();
+        Queue<Truck> moving = new LinkedList<>();
+
+        for (int t : truck_weights) {
+            waiting.offer(new Truck(t));
+        }
+
+        int answer = 0;
+        int weightOnBridge = 0;
+
+        // 기다리는 애가 있거나
+        // 움직이는 애가 있으면 계속 진행
+         System.out.println("==========================");
+        while(!waiting.isEmpty() || !moving.isEmpty()) {
+            answer++; // 최초 1 카운트..
+
+            System.out.println("------------------");
+            System.out.println(waiting.size());
+            System.out.println(moving.size());
+            System.out.println("------------------");
+
+            // 만약 움직이는 트럭이 없으면
+            if (moving.isEmpty()) {
+                Truck t = waiting.poll();
+                weightOnBridge += t.weight; // 대기에서 트럭하나를 꺼내와서 다리에 집어넣기.
+                moving.offer(t);
                 continue;
             }
-            // 넘어갈 수 있는 아이들 목록을 넣음
-            trucksOnBridge.offer(truck_weights[i]);
-        }
 
-        if (trucksOnBridge.isEmpty()) {
-            return 0;
-        }
-
-        int crosstime = 1;
-        int onBridgeWeight = 0;
-        int onCrossing = 0;
-        while(!trucksOnBridge.isEmpty()){
-
-            // 건널 트럭의 하중 예상치
-            int thisTruckSum = onBridgeWeight + trucksOnBridge.peek();
-
-            // 지금 다리위에 있는 트럭의 수가 다리의 길이보다 많은가?
-            // 혹은 예상치가 하중보다 큰가?
-            // 그렇다면 초기화
-            if ( onCrossing > bridge_length || thisTruckSum > weight) {
-                onBridgeWeight = 0;
-                onCrossing = 0;
-            // 예상치가 하중보다 작으면 건넌다.
-            } else if ( thisTruckSum < weight ) {
-
-                // 건너는 시간은 비어있는 다리를 건널때 length만큼 소모
-                if (onBridgeWeight < 1) {
-                    crosstime += bridge_length;
-                } else { // 비어있지 않으면 1만큼 소모
-                    crosstime += 1;
-                }
-                onBridgeWeight += trucksOnBridge.remove();
-                onCrossing++;
-            // 예상치가 하중과 같은 경우 건널 수 있다.
-            } else if ( thisTruckSum == weight ) {
-
-                // 비어있는 다리를 건널때 length만큼 소모
-                if (onBridgeWeight < 1) {
-                    crosstime += bridge_length;
-                } else { // 비어있지 않으면 1만큼 소모
-                    crosstime += 1;
-                }
-
-                trucksOnBridge.remove(); // 건넌다.
-                onBridgeWeight = 0; // 건넌뒤 다리의 무게는 초기화됨
-                onCrossing = 0;
+            // 움직이는 트럭이 있으면
+            for (Truck t : moving) {
+                t.moveTruck(); // 한단계 더 나가기
             }
+
+            // 한단계 나간 아이가 다리 길이보다 넘어가면
+            if (moving.peek().move > bridge_length) {
+                Truck t = moving.poll();
+                weightOnBridge -= t.weight; // 다리 무게에서 그 아이의 무게를 빼준다.
+            }
+
+            // 기다리는 아이가 있고 그 아이의 무게와 다리위 무게가 다리의 하중보다 작으면
+            if (!waiting.isEmpty() && weightOnBridge + waiting.peek().weight <= weight ) {
+                Truck t = waiting.poll(); // 기다리는 아이중에 하나를 데려와서 다리에 집어넣음.
+                weightOnBridge += t.weight;
+                moving.offer(t);
+            }
+
         }
 
-        return crosstime;
+        return answer;
     }
 
 }
